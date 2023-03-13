402cf78c1704d04a1c384eda6f79587a1e480ad8
Full Name <email@address.com>
2023-03-14 02:33:09 +0400

    d716a64093a7a5bbed4482667be1cd8d7d302e9a

--- .output/public/_nuxt/ProseBlockquote.3e2a26b9.js
+++
@@ -1,1 +0,0 @@
-import{x as o,o as t,y as r,v as c}from"./entry.a2c6e299.js";const s={};function n(e,l){return t(),r("blockquote",null,[c(e.$slots,"default")])}const f=o(s,[["render",n]]);export{f as default};
--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,5 @@
-git add .
-git commit -m "$(git log '--format=format:%H' master -1)"
-git subtree push --prefix=.output/public origin index --h
+git checkout master # you can avoid this line if you are in master...
+yarn generate
+git subtree split --prefix .output/public -b gh-pages # create a local gh-pages branch containing the splitted output folder
+git push -f origin gh-pages:gh-pages # force the push of the gh-pages branch to the remote gh-pages branch at origin
+git branch -D gh-pages # delete the local gh-pages because you will need it: ref
